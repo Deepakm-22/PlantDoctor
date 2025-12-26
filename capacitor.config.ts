91823400af04a880550cce3cd3216ce3edb10d87
deepak-271 <deepakgokul271@gmail.com>
2025-12-26 17:45:48 +0530

    the updated

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.example.plantdoctorai',
-  appName: 'Plant Doctor Ai',
+  appName: 'Plant Doctor AI',
   webDir: 'dist'
 };
 
